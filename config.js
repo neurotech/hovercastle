56d5de94880ce0d9aed6502722dac9a9c52c8ca9
Tim Douglas <neurotech@gmail.com>
2017-05-26 19:09:38 +1000

    Make it so.

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  discord: {
+    token: process.env.DISCORD_HOVERCASTLE_TOKEN
+  },
+  palette: {
+    blue: 0x5077F3,
+    red: 0xFF4949,
+    green: 0x12CE66,
+    yellow: 0xF7BA2A,
+    orange: 0xEE7F20,
+    purple: 0xBE87EC,
+    pink: 0xEE7ECD,
+    teal: 0x63E7C8
+  }
+};

8bab563026898e28091a7978a7ee0b57d624461a
Tim Douglas <neurotech@gmail.com>
2017-05-27 12:26:15 +1000

    Added forever, better logging

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const symbols = require('log-symbols');
+const dateFormat = require('dateformat');
 const config = require('./config');
 const router = require('./router');
 
@@ -6,7 +8,7 @@ const bot = new Discord.Client();
 bot.login(config.discord.token);
 
 bot.on('ready', () => {
-  console.log('Bot is now online.');
+  console.log(symbols.success, ` [${dateFormat()}] Hovercastle online.`);
 });
 
 // Watch for message
@@ -19,7 +21,15 @@ bot.on('message', message => {
 });
 
 // Re-login if bot goes offline
-setInterval(() => { if (bot.status === 1) bot.login(config.discord.token); }, 1000 * 30);
+setInterval(() => {
+  if (bot.status === 1) {
+    console.log(symbols.error, ` [${dateFormat()}] Hovercastle lost connection to Discord!`);
+    bot.login(config.discord.token)
+      .then(() => {
+        console.log(symbols.success, ` [${dateFormat()}] Hovercastle re-connected to Discord.`);
+      });
+  }
+}, 1000 * 30);
 
 // Log out on process exit/uncaught exception
 process.on('exit', () => { bot.destroy(); });

5257149d1048796589aa9c4095c614fb3fc9dfe0
Tim Douglas <neurotech@gmail.com>
2017-05-28 13:15:41 +1000

    Added overwatch command

---
+++ commands/overwatch.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+let pixel = './images/overwatch/pixel'
+let heroes = [
+  'ana',
+  'bastion',
+  'dva',
+  'genji',
+  'hanzo',
+  'junkrat',
+  'lucio',
+  'mccree',
+  'mei',
+  'mercy',
+  'pharah',
+  'reaper',
+  'reinhardt',
+  'roadhog',
+  'soldier-76',
+  'sombra',
+  'symmetra',
+  'torbjorn',
+  'tracer',
+  'widowmaker',
+  'winston',
+  'zarya',
+  'zenyatta'
+];
+
+module.exports = (message) => {
+  if (message.content.toLowerCase().startsWith('/overwatch pixel')) {
+    let chunked = message.content.split(' ');
+    let hero = chunked[2];
+    console.log(hero);
+  }
+};
